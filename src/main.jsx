38d05cf9d69336f1be051dc15a50d41e755ffb83
Andrew Fernando <andrew.fdo@hotmail.com>
2026-04-17 16:50:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 
 const queryClient = new QueryClient();
 
